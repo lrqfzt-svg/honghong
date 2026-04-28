ffb0f956c1a4f63cfacbfd6f50b0e882a118eba2
lrqfzt-svg <lrqfzt@gmail.com>
2026-04-28 18:26:59 +0800

    Fix: D1Database type error

--- src/lib/db/index-d1.ts
+++ src/lib/db/index-d1.ts
@@ -3,7 +3,7 @@ import * as schema from './schema-d1';
 
 // Cloudflare D1 数据库连接
 // 在 Pages 中通过 env.DB 访问
-export function getDB(env: { DB: D1Database }) {
+export function getDB(env: { DB: any }) {
   return drizzle(env.DB, { schema });
 }
 
